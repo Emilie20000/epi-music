275caabf90c0ee9f5d40e34dda420388942ccb15
emilie <emilie.le-lan@epitech.eu>
2025-04-05 18:28:55 +0200

    feat: add context for change the theme

--- client/src/components/ThemeSwitcher/ThemeSwitcher.js
+++ client/src/components/ThemeSwitcher/ThemeSwitcher.js
@@ -1,48 +1,26 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { useTheme } from "../../context/ThemeContext";
 
 const ThemeSwitcher = () => {
-    const [isDarkMode, setIsDarkMode] = useState(false);
-
-    useEffect(() => {
-        const savedTheme = localStorage.getItem("theme");
-        if (savedTheme) {
-            setIsDarkMode(savedTheme === "dark");
-        } else {
-            const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-            setIsDarkMode(prefersDark);
-        }
-    }, []);
-
-    const toggleTheme = () => {
-        setIsDarkMode(!isDarkMode);
-    };
-
-    useEffect(() => {
-        if (isDarkMode) {
-            document.body.classList.add("dark");
-            localStorage.setItem("theme", "dark");
-        } else {
-            document.body.classList.remove("dark");
-            localStorage.setItem("theme", "light");
-        }
-    }, [isDarkMode]);
+    const { isDark, toggleTheme } = useTheme();
 
     return (
         <div className="flex items-center">
             <label htmlFor="theme-toggle" className="flex items-center cursor-pointer">
                 <span className="mr-2 text-m text-gray-600 dark:text-gray-300">☀️</span>
                 <div className="relative">
+
                     <input
                         type="checkbox"
                         id="theme-toggle"
                         className="hidden"
-                        checked={isDarkMode}
+                        checked={isDark}
                         onChange={toggleTheme}
                     />
                     <div className="toggle-background w-12 h-6 bg-gray-300 dark:bg-gray-700 rounded-full"></div>
                     <div
                         className={`toggle-dot absolute top-0 left-0 w-6 h-6 bg-white rounded-full transition-transform duration-300 ${
-                            isDarkMode ? "transform translate-x-6" : ""
+                            isDark ? "transform translate-x-6" : ""
                         }`}
                     ></div>
                 </div>
@@ -53,4 +31,3 @@ const ThemeSwitcher = () => {
 };
 
 export default ThemeSwitcher;
-
